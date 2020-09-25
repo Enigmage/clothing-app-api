8079e4d5cb2bc3df41df1f0cf21d37538e43a935
Enigmage <alizaidi0786.az@gmail.com>
2020-09-26 02:37:05 +0530

    comments

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express()
+// Multer library for handling file uploads with multipart/form-data encoding
 const multer = require('multer');
 
 const PORT = 8000;
 
+// Path to where files will be saved
 const upload = multer({dest: __dirname+'/uploads/images'});
+
 app.use(express.static('public'));
 
 app.get('/', function(req,res) {
@@ -17,6 +20,7 @@ app.get('/hello', function(req, res) {
 })
 
 // upload.array() for multiple files
+// Route saves file using upload.single then returns json data about the file
 app.post('/process_image', upload.single('image'), function(req, res){
     if(req.file)
         res.json(req.file);

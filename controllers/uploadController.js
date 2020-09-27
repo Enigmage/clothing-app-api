2d473f58256dfafee54a7e6d73ea12e1609d2a94
Enigmage <alizaidi0786.az@gmail.com>
2020-09-27 19:52:35 +0530

    few changes

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,26 +1,22 @@
 const path = require('path') 
 
 exports.process_image =  (req,res) => {
-
-//adding file validation
-    
-    if(req.file == undefined)
-throw new Error("File cannot be empty");
-    
-    var extension = path.extname(req.file.originalname);
-    console.log(extension);
-    
-    if(extension == '.sh' || extension == '.bashrc')
-	throw new Error("Begone Script Kiddie!");
-    
-    else if(extension != '.jpg' && extension != '.jpeg' && extension != '.png' && extension != '.raw'){
-	throw new Error("Only images are allowed");
-    }
-    else if(parseInt(req.file.size) > 10000000) //file size limit = 10mb
 	
-	throw new Error("File size is greater than the limit")
+//adding file validation
+	
+    if(!req.file) {
+		throw new Error("File cannot be empty");
+		return;
+	}
+    const extension = path.extname(req.file.originalname);
     
-    else if(req.file)
-	res.json(req.file)
-    }
+    if(extension != '.jpg' && extension != '.jpeg' && extension != '.png' && extension != '.raw')	
+		throw new Error("Only images are allowed");
+
+    else if(parseInt(req.file.size) > 10000000) //file size limit = 10mb
+		throw new Error("File size is greater than the limit")
+    
+	else
+		res.json(req.file)
+}
 

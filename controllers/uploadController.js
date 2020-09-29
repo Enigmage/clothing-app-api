a3734cdbb2fbb981acb20d7a0a834103bb634515
yash07sharma <yash2307sharma@gmail.com>
2020-09-29 17:23:24 +0530

    Add model for dominant color extraction

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -2,7 +2,7 @@ const { py_connect } = require("../utils/connectors")
 
 exports.process_image =  (req,res) => {
     if(req.file) {
-        req.file.demoProperties = py_connect(req.file.file_destination , req.file.filename)
+        req.file.demoProperties = JSON.parse(py_connect(req.file.destination , req.file.filename))
         res.json(req.file)
     } else
 	throw new Error("File cannot be empty.")
